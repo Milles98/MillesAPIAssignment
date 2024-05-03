0c0b5956a86f775d9aac8c04b8166dd7bf04f98a
Mille Elfver <mille.elfver98@gmail.com>
2024-05-03 09:22:34 +0200

    new feature, added adpostdto to jsonignore isdeleted for httppost

---
+++ AdvertisementAPI/Models/DTO/AdPostDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AdvertisementAPI.Models.DTO
+{
+    public class AdPostDTO
+    {
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; } = null!;
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        [JsonIgnore]
+        public int IsDeleted { get; set; }
+    }
+}
